e0673cf8efa072426bc2297f2d9536c8291625ca
Ryan Cleary <ryancleary@Ryans-MacBook-Pro.local>
2017-08-05 19:28:19 -0500

    updated workspace settings, npm packages, etc.

--- server.js
+++ server.js
@@ -1,26 +1,22 @@
-'use strict';
+const express = require('express');
+const mongo = require('mongodb');
+const routes = require('./app/routes/index.js');
 
-var express = require('express');
-var mongo = require('mongodb');
-var routes = require('./app/routes/index.js');
+const app = express();
 
-var app = express();
+mongo.connect('mongodb://localhost:27017/clementinejs', (err, db) => {
+  if (err) {
+    throw new Error('Database failed to connect!');
+  } else {
+    console.log('Successfully connected to MongoDB on port 27017.');
+  }
 
-mongo.connect('mongodb://localhost:27017/clementinejs', function (err, db) {
+  app.use('/public', express.static(`${process.cwd()}/public`));
+  app.use('/controllers', express.static(`${process.cwd()}/app/controllers`));
 
-   if (err) {
-      throw new Error('Database failed to connect!');
-   } else {
-      console.log('Successfully connected to MongoDB on port 27017.');
-   }
+  routes(app, db);
 
-   app.use('/public', express.static(process.cwd() + '/public'));
-   app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-
-   routes(app, db);
-
-   app.listen(3000, function () {
-      console.log('Node.js listening on port 3000...');
-   });
-
+  app.listen(3000, () => {
+    console.log('Node.js listening on port 3000...');
+  });
 });
